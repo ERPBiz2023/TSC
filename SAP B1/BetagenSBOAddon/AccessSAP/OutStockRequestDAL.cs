833fa7bab6cdd536d847bafe2db96d074c9e9236
Nghia Nguyen <nghia.nhan.swe@gmail.com>
2023-04-19 17:34:05 +0700

    Nghia Nguyen 19-04-2023 Check code

---
+++ SAP B1/BetagenSBOAddon/AccessSAP/OutStockRequestDAL.cs
@@ -0,0 +1,102 @@
+﻿using GTCore.SAP.DIAPI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetagenSBOAddon.AccessSAP
+{
+    public class OutStockRequestDAL
+    {
+        public int CreateInventoryTrannsferRequest(string stockNo, ref string message)
+        {
+            int lRetCode = -1, lErrCode;
+            var query = string.Format(Querystring.sp_LoadOutStockRequestByID, stockNo);
+            Hashtable data = null;
+
+            var queryDetail = string.Format(Querystring.sp_LoadOutStockRequestDetailByID, stockNo);
+            Hashtable[] dataDetails = null;
+            using (var connection = Globals.DataConnection)
+            {
+                var errorstr = string.Empty;
+                data = connection.ExecQueryToHashtable(query, out errorstr);
+                dataDetails = connection.ExecQueryToArrayHashtable(queryDetail, out errorstr);
+                connection.Dispose();
+            }
+
+            if (data == null || (dataDetails == null || dataDetails.Length <= 0))
+            {
+                message = "Data is not loaded";
+                return -1;
+            }
+
+            try
+            {
+
+                var ret = DIConnection.Instance.Connect(ref message);
+                if (ret)
+                {
+                    var oStockTransfer = (SAPbobsCOM.StockTransfer)DIConnection.Instance.Company.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oInventoryTransferRequest);
+                    DateTime dtStockDate;
+                    var requestNo = data["StockNo"].ToString();
+                    if (DateTime.TryParse(data["StockDate"].ToString(), out dtStockDate))
+                    {
+                        oStockTransfer.DocDate =  dtStockDate;
+                        oStockTransfer.TaxDate = dtStockDate;
+                    }
+
+                    oStockTransfer.Comments = string.Format("Yêu cầu chuyển kho nội bộ:{0}", requestNo);
+                    oStockTransfer.UserFields.Fields.Item("U_DescEN").Value = string.Format("Internal transfer Req:{0}", requestNo);// "Internal transfer Req: " + ReqNo);
+                                                                                                                                    //BS_STOCKOUTREQUEST_DETAIL_LoadbyID
+                    oStockTransfer.FromWarehouse = data["FromWhsCode"].ToString();
+                    oStockTransfer.ToWarehouse = data["ToWhsCode"].ToString();
+                    oStockTransfer.UserFields.Fields.Item("U_SoPhieu").Value = requestNo;
+                    oStockTransfer.UserFields.Fields.Item("U_FromBIN").Value = string.IsNullOrEmpty(data["AbsEntry"].ToString()) ? "-1" : data["AbsEntry"].ToString();
+                    oStockTransfer.UserFields.Fields.Item("U_FromBinCode").Value = string.IsNullOrEmpty(data["BinCode"].ToString()) ? "" : data["BinCode"].ToString(); //RuntimeHelpers.GetObjectValue(Interaction.IIf(table1.Rows[index1]["BinCode"] == DBNull.Value, (object)"", RuntimeHelpers.GetObjectValue(table1.Rows[index1]["BinCode"])));
+                    oStockTransfer.UserFields.Fields.Item("U_TOBIN").Value = string.IsNullOrEmpty(data["AbsEntry1"].ToString()) ? "-1" : data["AbsEntry1"].ToString(); //RuntimeHelpers.GetObjectValue(Interaction.IIf(table1.Rows[index1]["AbsEntry1"] == DBNull.Value, (object)-1, RuntimeHelpers.GetObjectValue(table1.Rows[index1]["AbsEntry1"])));
+                    oStockTransfer.UserFields.Fields.Item("U_ToBinCode").Value = string.IsNullOrEmpty(data["BinCode1"].ToString()) ? "" : data["BinCode1"].ToString(); //RuntimeHelpers.GetObjectValue(Interaction.IIf(table1.Rows[index1]["BinCode1"] == DBNull.Value, (object)"", RuntimeHelpers.GetObjectValue(table1.Rows[index1]["BinCode1"])));
+
+                    var index = 0;
+                    foreach (var detail in dataDetails)
+                    {
+                        oStockTransfer.Lines.ItemCode = detail["ItemCode"].ToString();
+                        oStockTransfer.Lines.Quantity = double.Parse(detail["Quantity"].ToString());
+                        oStockTransfer.Lines.FromWarehouseCode = data["FromWhsCode"].ToString();
+                        oStockTransfer.Lines.WarehouseCode = data["ToWhsCode"].ToString();
+                        oStockTransfer.Lines.UserFields.Fields.Item("U_BatchNo").Value = detail["LotNo"].ToString();
+
+                        if (index != dataDetails.Length - 1)
+                            oStockTransfer.Lines.Add();
+                        index++;
+                    }
+                    lRetCode = oStockTransfer.Add();
+                    if (lRetCode != 0)
+                    {
+                        DIConnection.Instance.Company.GetLastError(out lErrCode, out message);
+
+                        lErrCode = - 1;
+                    }
+                    else
+                    {
+
+                        lErrCode = 1;
+                    }
+                    DIConnection.Instance.DIDisconnect();
+                    return lErrCode;
+
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            catch (Exception ex)
+            {
+                message = string.Format("Error {0}", ex.Message);
+                return -1;
+            }
+        }
+    }
+}
---
+++ SAP B1/BetagenSBOAddon/Querystring.cs
@@ -0,0 +1,112 @@
+﻿using GTCore.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetagenSBOAddon
+{
+    public class Querystring
+    {
+        private static string CallStoreBySystem(string query, string param = "")
+        {
+            if (CoreSetting.System == SystemType.SAP_HANA)
+            {
+                return "CALL \"" + query + "\" (" + param +")";
+            }
+            else
+            {
+                return "EXEC " + query + " " + param;//, query, param);
+            }
+        }
+        public static string sp_LoadOutStockRequest
+        {
+           
+            get
+            {
+                return CallStoreBySystem("USP_BS_STOCKOUTREQUEST", "'{0}','{1}','{2}', {3}");
+                //if (CoreSetting.System == SystemType.SAP_HANA)
+                //    return "CALL \"USP_BS_STOCKOUTREQUEST\" ('{0}','{1}','{2}', {3})";
+                //else
+                //    return "EXEC USP_BS_STOCKOUTREQUEST '{0}','{1}','{2}', {3}";
+            }
+        }
+
+        public static string sp_DeleteOutStockRequest
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_STOCKOUTREQUEST2_DELETE", "'{0}'");
+                //if (CoreSetting.System == SystemType.SAP_HANA)
+                //    return "CALL \"USP_BS_STOCKOUTREQUEST2_DELETE\" ('{0}')";
+                //else
+                //    return "EXEC USP_BS_STOCKOUTREQUEST2_DELETE '{0}'";
+            }
+        }
+        public static string sp_NotificationUpdateStock
+        {
+            get
+            {
+                return CallStoreBySystem("usp_Notification_UpdateStock");
+                //if (CoreSetting.System == SystemType.SAP_HANA)
+                //    return "CALL \"usp_Notification_UpdateStock\" ()";
+                //else
+                //    return "EXEC usp_Notification_UpdateStock ";
+            }
+        }
+
+        public static string sp_LoadOutStockRequestByID
+        {
+            get
+            {
+                return CallStoreBySystem("usp_BS_STOCKOUTREQUEST_LoadbyID", "'{0}'");
+                //if (CoreSetting.System == SystemType.SAP_HANA)
+                //    return "CALL \"usp_BS_STOCKOUTREQUEST_LoadbyID\" ('{0}')";
+                //else
+                //    return "EXEC usp_BS_STOCKOUTREQUEST_LoadbyID '{0}'";
+            }
+        }
+        public static string sp_LoadOutStockRequestDetailByID
+        {
+            get
+            {
+                return CallStoreBySystem("usp_BS_STOCKOUTREQUEST_DETAIL_LoadbyID", "'{0}'");
+                //if (CoreSetting.System == SystemType.SAP_HANA)
+                //    return "CALL \"usp_BS_STOCKOUTREQUEST_DETAIL_LoadbyID\" ('{0}')";
+                //else
+                // return "EXEC usp_BS_STOCKOUTREQUEST_DETAIL_LoadbyID '{0}'";
+            }
+        }
+
+        public static string sp_OutStockRequestApplySAP
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_STOCKOUTREQUEST_APPLY_SAP", "'{0}', {1}");
+            }
+        }
+        public static string sp_OutStockRequestConfirm
+        {
+            get
+            {
+                return CallStoreBySystem("usp_BS_InventoryTransferReq_Confirm", "'{0}', {1}");
+            }
+        }
+
+        public static string sp_GetWarehouses
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_OWHS", "");
+            }
+        }
+        public static string sp_GetBins
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_OWHS", "");
+            }
+        }
+    }
+}
