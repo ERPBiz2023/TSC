dd424d3aa3ac20711f367c40eb8aef5ef00b321b
Nghia Nguyen <nghia.nhan.swe@gmail.com>
2023-05-02 07:50:27 +0700

    Nghia

--- SAP B1/BetagenSBOAddon/SystemForm/SalesQuotationExt.b1f.cs
+++ SAP B1/BetagenSBOAddon/SystemForm/SalesQuotationExt.b1f.cs
@@ -41,6 +41,7 @@ namespace BetagenSBOAddon.SystemForm
             //btnApplyDiscount..DisplayDesc = true;
             btnApplyDiscount.Item.DisplayDesc = false;
             btnApplyDiscount.Item.Top = _2Button.Item.Top;
+            btnApplyDiscount.Item.Height = _2Button.Item.Height;
             btnApplyDiscount.Item.Left = _2Button.Item.Left + 70;
             btnApplyDiscount.Item.Width = 80;
             btnApplyDiscount.Caption = "Apply Discount";
