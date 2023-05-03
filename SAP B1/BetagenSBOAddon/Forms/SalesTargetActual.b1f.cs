9220c7e768e4d2f5c7c52726d6df518b38d645ba
Nghia Nguyen <nghia.nhan.swe@gmail.com>
2023-05-03 15:11:01 +0700

    Nghia Nguyen 03-05-2023 check form

---
+++ SAP B1/BetagenSBOAddon/Forms/SalesTargetActual.b1f.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAPbouiCOM.Framework;
+
+namespace BetagenSBOAddon.Forms
+{
+    [FormAttribute("BetagenSBOAddon.Forms.SalesTargetActual", "Forms/SalesTargetActual.b1f")]
+    class SalesTargetActual : UserFormBase
+    {
+        public SalesTargetActual()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_0").Specific));
+            this.ComboBox0 = ((SAPbouiCOM.ComboBox)(this.GetItem("Item_1").Specific));
+            this.StaticText1 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_2").Specific));
+            this.ComboBox1 = ((SAPbouiCOM.ComboBox)(this.GetItem("Item_3").Specific));
+            this.StaticText2 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_4").Specific));
+            this.ComboBox2 = ((SAPbouiCOM.ComboBox)(this.GetItem("Item_5").Specific));
+            this.StaticText3 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_6").Specific));
+            this.ComboBox3 = ((SAPbouiCOM.ComboBox)(this.GetItem("Item_7").Specific));
+            this.StaticText4 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_8").Specific));
+            this.ComboBox4 = ((SAPbouiCOM.ComboBox)(this.GetItem("Item_9").Specific));
+            this.StaticText5 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_10").Specific));
+            this.ComboBox5 = ((SAPbouiCOM.ComboBox)(this.GetItem("Item_11").Specific));
+            this.StaticText6 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_12").Specific));
+            this.ComboBox6 = ((SAPbouiCOM.ComboBox)(this.GetItem("Item_13").Specific));
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_14").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+            this.LoadAfter += new LoadAfterHandler(this.Form_LoadAfter);
+
+        }
+
+        private SAPbouiCOM.StaticText StaticText0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private SAPbouiCOM.ComboBox ComboBox0;
+        private SAPbouiCOM.StaticText StaticText1;
+        private SAPbouiCOM.ComboBox ComboBox1;
+        private SAPbouiCOM.StaticText StaticText2;
+        private SAPbouiCOM.ComboBox ComboBox2;
+        private SAPbouiCOM.StaticText StaticText3;
+        private SAPbouiCOM.ComboBox ComboBox3;
+        private SAPbouiCOM.StaticText StaticText4;
+        private SAPbouiCOM.ComboBox ComboBox4;
+        private SAPbouiCOM.StaticText StaticText5;
+        private SAPbouiCOM.ComboBox ComboBox5;
+        private SAPbouiCOM.StaticText StaticText6;
+        private SAPbouiCOM.ComboBox ComboBox6;
+        private SAPbouiCOM.Button Button0;
+
+        private void Form_LoadAfter(SAPbouiCOM.SBOItemEventArg pVal)
+        {
+           
+
+        }
+    }
+}
---
+++ SAP B1/GLPosting.b1f.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GTCore.Forms;
+using SAPbouiCOM.Framework;
+
+namespace BetagenSBOAddon.Forms
+{
+    [FormAttribute("BetagenSBOAddon.Forms.GLPosting", "Forms/GLPosting.b1f")]
+    class GLPosting : UserFormBase
+    {
+        private GLPosting()
+        {
+        }
+
+        private static AddonUserForm information;
+        public static AddonUserForm Information
+        {
+            get
+            {
+                if (information == null)
+                {
+                    information = new AddonUserForm();
+                    information.FormID = "GLPosting_F";
+                    information.MenuID = "GLPosting_M";
+                    information.MenuName = "Postting/Re-Allocate GL";
+                    information.ParentID = "1536"; // Inventory Transactions
+                }
+                return information;
+            }
+        }
+        private static GLPosting instance;
+
+        public static bool IsFormOpen = false;
+        public static void ShowForm()
+        {
+            if (instance == null)
+            {
+                instance = new GLPosting();
+                instance.Show();
+                IsFormOpen = true;
+            }
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_0").Specific));
+            this.StaticText1 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_1").Specific));
+            this.StaticText2 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_2").Specific));
+            this.cbbMon = ((SAPbouiCOM.ComboBox)(this.GetItem("cbbMon").Specific));
+            this.cbbYear = ((SAPbouiCOM.ComboBox)(this.GetItem("cbbYea").Specific));
+            this.cbbGrp = ((SAPbouiCOM.ComboBox)(this.GetItem("cbbGrp").Specific));
+            this.btnPost = ((SAPbouiCOM.Button)(this.GetItem("btnPo").Specific));
+            this.btnCancel = ((SAPbouiCOM.Button)(this.GetItem("btnCan").Specific));
+            this.btnCancel.ClickAfter += new SAPbouiCOM._IButtonEvents_ClickAfterEventHandler(this.btnCancel_ClickAfter);
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.StaticText StaticText0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private SAPbouiCOM.StaticText StaticText1;
+        private SAPbouiCOM.StaticText StaticText2;
+        private SAPbouiCOM.ComboBox cbbMon;
+        private SAPbouiCOM.ComboBox cbbYear;
+        private SAPbouiCOM.ComboBox cbbGrp;
+        private SAPbouiCOM.Button btnPost;
+        private SAPbouiCOM.Button btnCancel;
+
+        private void btnCancel_ClickAfter(object sboObject, SAPbouiCOM.SBOItemEventArg pVal)
+        {
+            instance = null;
+            IsFormOpen = false;
+        }
+    }
+}
