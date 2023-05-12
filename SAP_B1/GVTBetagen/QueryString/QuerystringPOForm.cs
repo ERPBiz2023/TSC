0855254232cb43d5bc85e7036c139ba32dc4ef9a
Nghia Nguyen <nghia.nhan.swe@gmail.com>
2023-05-12 09:24:44 +0700

    nghia - commit source

---
+++ SAP_B1/GVTBetagen/QueryString/QuerystringPOForm.cs
@@ -0,0 +1,92 @@
+﻿using GTCore.Config;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVTBetagen
+{
+    public partial class Querystring
+    {
+
+
+        /// <summary>
+        /// usp_BS_PO_LoadbyEntry "{0}"
+        /// </summary>
+        public static string sp_GetPOByDocEntry
+        {
+            get
+            {
+                return CallStoreBySystem("usp_BS_PO_LoadbyEntry", "{0}");
+            }
+        }
+
+        /// <summary>
+        /// USP_BS_PO_DateAllocate_LoadItem
+        /// </summary>
+        public static string sp_GetAllItemToCombobox
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_PO_DateAllocate_LoadItem", "{0}");
+            }
+        }
+
+        /// <summary>
+        /// USP_BS_PO_DateAllocate_LoadBIN
+        /// </summary>
+        public static string sp_GetAllBinToCombobox
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_PO_DateAllocate_LoadBIN", "");
+            }
+        }
+
+        /// <summary>
+        /// USP_BS_PO_DateAllocate_LoadPOLotNobyEntry "'{0}'"
+        /// </summary>
+        public static string sp_LoadPOAllocate
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_PO_DateAllocate_LoadPOLotNobyEntry", "'{0}'");
+            }
+        }
+
+        /// <summary>
+        /// usp_BS_PO_DateAllocate_ImportInfo_DeleteBefAdd "{0}"
+        /// </summary>
+        public static string sp_POAllocateImportInfo_DeleteBefAdd
+        {
+            get
+            {
+                return CallStoreBySystem("usp_BS_PO_DateAllocate_ImportInfo_DeleteBefAdd", "{0}");
+            }
+        }
+
+        /// <summary>
+        /// usp_BS_PO_DateAllocate_ImportInfo_Add "{0}, '{1}', '{2}', '{3}', '{4}', {5}"
+        /// </summary>
+        public static string sp_POAllocateImportInfo_Add
+        {
+            get
+            {
+                return CallStoreBySystem("usp_BS_PO_DateAllocate_ImportInfo_Add", "{0}, '{1}', '{2}', '{3}', '{4}', {5}");
+            }
+        }
+
+        /// <summary>
+        /// usp_BS_PO_DateAllocate_ImportInfo_POLotNo_Add "{0}, '{1}'
+        /// </summary>
+        public static string sp_POAllocateImportInfo_LotNoAdd
+        {
+            get
+            {
+                return CallStoreBySystem("usp_BS_PO_DateAllocate_ImportInfo_POLotNo_Add", "{0}, '{1}'");
+            }
+        }
+    }
+}
---
+++ SAP_B1/GVTBetagen/QueryString/QuerystringSalesTarget.cs
@@ -0,0 +1,54 @@
+﻿using GTCore.Config;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVTBetagen
+{
+    public partial class Querystring
+    {
+
+        /// <summary>
+        /// USP_BS_SalesManager_LoadbyUserId "'{0}'"
+        /// </summary>
+        public static string sp_GetSaleManagerByUser
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_SalesManager_LoadbyUserId", "'{0}'");
+            }
+        }
+
+        /// <summary>
+        /// "USP_BS_KAASM_LoadbyUserId "'{0}', '{1}'"
+        /// </summary>
+        public static string sp_GetKA_ASMByUser
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_KAASM_LoadbyUserId", "'{0}', '{1}'");
+            }
+        }
+
+        /// <summary>
+        /// USP_BS_SalesSup_LoadbyUserId "'{0}', '{1}', '{2}'"
+        /// </summary>
+        public static string sp_GetSalesSupByUser
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_SalesSup_LoadbyUserId", "'{0}', '{1}', '{2}'");
+            }
+        }
+        public static string sp_GetTeamLeaderByUser
+        {
+            get
+            {
+                return CallStoreBySystem("USP_BS_TeamLead_LoadbyUserId", "'{0}', '{1}', '{2}', '{3}'");
+            }
+        }
+    }
+}
